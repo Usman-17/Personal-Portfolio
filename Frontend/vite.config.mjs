b47a1d4b6ca779a8c5d27d53fd97dd856ecb7f19
Usman-17 <usmandev580@gmail.com>
2025-05-07 12:38:44 +0500

    update

--- Frontend/vite.config.mjs
+++ Frontend/vite.config.mjs
@@ -14,7 +14,8 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: "http://localhost:8000",
+        // target: "http://localhost:8000",
+        target: "https://personal-portfolio-six-iota-40.vercel.app/",
         changeOrigin: true,
         secure: false,
       },
